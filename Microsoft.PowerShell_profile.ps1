85809e94678574c829fde17c8cf6f21ef0bb709d
abc123id <146668624+abc123id@users.noreply.github.com>
2023-10-25 17:28:59 -0400

    WARNING: THIS IS FOR POWERSHELL 5!!! NOT PWSH

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
 # Load Oh-My-Posh
-& ([ScriptBlock]::Create((oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\catppuccin.omp.json" --print) -join "`n"))
+& ([ScriptBlock]::Create((oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\clean-detailed.omp.json" --print) -join "`n"))
 
 # Import the Chocolatey Profile that contains the necessary code to enable tab-completions to function for `choco`. Be aware that if you are missing these lines from your profile, tab completion for `choco` will not function. See https://ch0.co/tab-completion for details.
 
@@ -11,10 +11,6 @@ if (Test-Path($ChocolateyProfile)) {
 # Import and load TerminalIcons
 Import-Module -Name Terminal-Icons
 
-# PsFzf option loading
-Import-Module PSFzf
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
-
 
 # PSReadLine option loading
 Import-Module PSReadLine
@@ -24,21 +20,10 @@ Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
 
-# Somethin to do with python idk
-$env:PYTHONIOENCODING="utf-8"
-
-# Aliases
-
+# Extra commands
 Set-Alias vim nvim
 Set-Alias ll ls
-Set-Alias clear cls
 Set-Alias ref cls
 Set-Alias refresh cls
 Set-Alias cc cls
 Set-Alias ranger lf
-
-#Loaded Output (Cannot contain spaces)
-
-echo [OK]SystemLoaded
-
-#Profile end

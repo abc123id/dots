7035d4ff6345727e6af58bdaf061a4ee155cdef0
abc123id <connorearnest@protonmail.com>
2023-11-24 20:28:39 -0500

    update userprofile

--- powershells/user_profile.ps1
+++ powershells/user_profile.ps1
@@ -1,35 +1,5 @@
-# ==> Load OMP Config
-& ([ScriptBlock]::Create((oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\uew.omp.json" --print) -join "`n"))
-
-# ==> Import the Chocolatey Profile that contains the necessary code to enable tab-completions to function for `choco`. Be aware that if you are missing these lines from your profile, tab completion for `choco` will not function. See https://ch0.co/tab-completion for details.
-
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
-
-# ==> Import and load Terminal-Icons
-Import-Module -Name Terminal-Icons
-
-# ==> Import Z, Directory Jumper
-Import-Module -Name z
-
-# ==> PsFzf option loading
-Import-Module PSFzf
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
-
-# ==> PSReadLine option loading
-Import-Module PSReadLine
-Set-PSReadLineOption -EditMode Emacs
-Set-PSReadLineOption -BellStyle None
-Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
-Set-PSReadLineOption -PredictionSource History
-Set-PSReadLineOption -PredictionViewStyle ListView
-
-# ==> Somethin to do with python idk
-$env:PYTHONIOENCODING="utf-8"
-
-# ==> Aliases
+#+BEGIN_SRC powershell
+ ## Aliases
 
 Set-Alias vim nvim
 Set-Alias ll ls
@@ -43,13 +13,40 @@ Set-Alias v nvim
 Set-Alias neo neofetch
 Set-Alias htop ntop
 
-# ==> Thefuck alias
-Invoke-Expression "$(thefuck --alias)"
-
-# ==> Import pwsh-syntax-highlighting
+ ## Imports
+Import-Module -Name Terminal-Icons
+Import-Module -Name z
+Import-Module PSFzf
+Import-Module PSReadLine
 Import-Module syntax-highlighting
 
-# ==> Functions
+ ## Prompt
+& ([ScriptBlock]::Create((oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\uew.omp.json" --print) -join "`n"))
+
+ ## Cocolately Tab-Autocompletion
+
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+ ## PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+ ## PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+ ## Python
+$env:PYTHONIOENCODING="utf-8"
+
+ ## Command Correction
+Invoke-Expression "$(thefuck --alias)"
+
+ ## Functions
 function profile {
     vim C:\users\conno\.config\powershell\user_profile.ps1
   }
@@ -59,6 +56,5 @@ function binds {
 function cprofile {
     code C:\users\conno\.config\powershell\user_profile.ps1 
   }
-#Loaded Output (Cannot contain spaces)
 
-#Profile end
+ ## End Of Configuration
